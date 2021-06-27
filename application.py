4a6ad99ebdd0842c1b2a7b902178a03ebb131053
Apoorv Tyagi <tyagi.apoorv133@gmail.com>
2021-06-27 10:46:27 +0530

    Tested all files

--- application.py
+++ application.py
@@ -4,15 +4,18 @@ import tweet_reply
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 
-def hello():
+
+def job():
     tweet_reply.respondToTweet('tweet_ID.txt')
     print("Success")
 
+
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=hello, trigger="interval", seconds=60)
+scheduler.add_job(func=job, trigger="interval", seconds=60)
 scheduler.start()
 
 application = Flask(__name__)
+
 
 @application.route("/")
 def index():
@@ -20,7 +23,6 @@ def index():
 
 
 atexit.register(lambda: scheduler.shutdown())
-
 
 if __name__ == "__main__":
     application.run(port=5000, debug=True)
